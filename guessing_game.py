9a710c8b6605ffd660bf7bf355f3263eb2346e22
Saeed Gholami <saeed.ghollami@gmail.com>
2024-04-27 22:20:56 +0330

    Create number guessing game.

--- guessing_game.py
+++ guessing_game.py
@@ -1,2 +1,30 @@
+#-----------------
+# Guessing Game
+# ----------------
+import random
 
-print("hello world!")
+rand_number = random.randrange(1, 100)
+is_win = False
+
+print("I am number between 1 to 99")
+print("Try to guess me.")
+
+print(rand_number)
+
+for i in range(10):
+    user_guess = int(input("Your guess > "))
+
+    # Compare user input with random number.
+    if user_guess > rand_number:
+        print("Try lower.")
+    elif user_guess < rand_number:
+        print("Try higher")
+    else:
+        print("You win.")
+        is_win = True
+        break
+
+# if the user didn't win.
+if is_win == False:
+    print("You didn't win.")
+
